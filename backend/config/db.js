6b2f0a215c4ac6be4c1c75491ccc05f61f3a41e6
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2026-01-30 22:27:38 +0100

    backend db fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,15 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+
+  connectTimeout: 20000,
 });
 
 module.exports = pool.promise();
