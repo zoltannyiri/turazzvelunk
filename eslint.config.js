a5c5559593a1e8ecbceca41bcdcc76b98a4bf62c
Nyiri Zoltán <zoltan.nyiri02@gmail.com>
2026-08-24 18:51:27 +0200

    warn changes

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
 ])
